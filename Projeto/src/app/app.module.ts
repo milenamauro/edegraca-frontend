1aeb8b0125d8921522dd8b879be98d9536906978
milenamauro <milena.93@hotmail.com>
2021-09-01 13:28:10 -0300

    edg front

--- Projeto/src/app/app.module.ts
+++ Projeto/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,10 @@ import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { LogarComponent } from './logar/logar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { Menu2Component } from './menu2/menu2.component';
+import { ModalPostComponent } from './modal-post/modal-post.component';
+import { FormsModule } from '@angular/forms';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +19,16 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
     MenuComponent,
     RodapeComponent,
     LogarComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    Menu2Component,
+    ModalPostComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Projeto/src/app/model/UsuarioLogin.ts
@@ -0,0 +1,9 @@
+
+export class UsuarioLogin {
+    public id: number
+    public email: string
+    public nome: string
+    public senha: string
+    public empresa: boolean
+    public token: string
+}
